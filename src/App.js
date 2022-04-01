b452c6a4572694cc96f6b7e319ce055df7d25f61
Hippolyte EZIAN <polarusdev@gmail.com>
2022-04-01 15:40:13 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tasks: [],
+      value: "",
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let { tasks, value } = this.state;
+    let taskId = Math.floor(Math.random() * 10000).toString();
+    if (value.trim().length > 0) {
+      tasks.push({
+        title: value,
+        id: taskId,
+        done: false,
+      });
+      this.setState({ tasks: tasks, value: "" });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>To Do List</h1>
+        <form
+          onSubmit={(event) => {
+            this.handleSubmit(event);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <input
+            type="text"
+            value={this.state.value}
+            placeholder="name"
+            onInput={(event) => {
+              this.setState({ value: event.target.value });
+            }}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <ul>
+          {this.state.tasks.map(({ id, title, done }, index) => {
+            return (
+              <div
+                key={id}
+                style={
+                  done
+                    ? { backgroundColor: "green", color: "White" }
+                    : { backgroundColor: "white", color: "black" }
+                }
+              >
+                <li>{title}</li>
+                <button
+                  onClick={() => {
+                    let { tasks } = this.state;
+                    tasks[index].done = !done;
+                    this.setState({ tasks: tasks });
+                  }}
+                >
+                  Done
+                </button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
